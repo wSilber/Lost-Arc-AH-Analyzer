c07530578e811c5444e2553284113215b0853427
wSilber <silberstein75@gmail.com>
2023-05-25 15:48:47 -0400

    Removed URL

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import { scrapeAllCategories } from './scraper.js';
 
 ;(async function() {
 
-    const url = 'https://www.lostarkmarket.online/api/export-market-live/North America East?subcategory=Honing Materials&categories=Enhancement Material&items=great-honor-leapstone-2'
-
     await scrapeAllCategories();
 
 })()
